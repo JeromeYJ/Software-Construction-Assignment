726db83f82fd6fe10d325617e774aad1a63a4df0
JeromeYJ <93578822+JeromeYJ@users.noreply.github.com>
2023-03-12 20:04:42 +0800

    第三次作业

---
+++ assignment3/AreaSum/Program.cs
@@ -0,0 +1,50 @@
+﻿using Shape;
+
+namespace AreaSum
+{
+    class ShapeFactory     //工厂类
+    {
+        public static IShape CreateShape(string type, double side1, double side2, double side3)
+        {
+            if (type == "Rectangle")
+            {
+                Console.WriteLine("Create a Rectangle object!");
+                return new Rectangle(side1, side2);
+            }
+            else if (type == "Square")
+            {
+                Console.WriteLine("Create a Square object!");
+                return new Square(side1);
+            }
+            else if (type == "Triangle")
+            {
+                Console.WriteLine("Create a Triangle object!");
+                return new Triangle(side1, side2, side3);
+            }
+            else
+                throw new SystemException("出错！");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] shapeName = { "Rectangle", "Square", "Triangle" };
+            IShape shape;
+            int typeIndex;
+            double side1, side2, side3;
+            double areaSum = 0;
+            for(int i = 0; i < 10; i++)
+            {
+                Random rd = new Random(Guid.NewGuid().GetHashCode());
+                typeIndex = rd.Next(0, 3);
+                side1 = rd.Next(1, 20);
+                side2 = rd.Next(1, 20);
+                side3 = side1 + side2 - 1;
+                shape = ShapeFactory.CreateShape(shapeName[typeIndex], side1, side2, side3);  //通过工厂类创建各类对象
+                areaSum += shape.CalArea();
+            }
+            Console.WriteLine("随机生成的10个形状对象的面积之和为：{0}", areaSum);
+        }
+    }
+}
---
+++ assignment3/Shape/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+
+namespace Shape
+{
+    public interface IShape
+    {
+        double CalArea();
+        bool IsValid();
+    }
+    public class Rectangle:IShape
+    {
+        protected double width;
+        protected double height;
+
+        public Rectangle(double width,double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public double Width
+        {
+            get => width;
+            set => width = value;
+        }
+        public double Height
+        {
+            get => height;
+            set => height = value;
+        }
+        public double CalArea()
+        {
+            if (!IsValid())
+                throw new SystemException("输入的长宽不合法！");
+            return width * height;
+        }
+        public bool IsValid()
+        {
+            return (width > 0 && height > 0);
+        }
+    }
+    public class Square:Rectangle
+    {
+        public Square(double side) : base(side, side) {}
+    }
+    public class Triangle:IShape
+    {
+        double side1, side2, side3;
+
+        public Triangle(double side1,double side2,double side3)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+            this.side3 = side3;
+        }
+        public double Side1
+        {
+            get => side1;
+            set => side1 = value;
+        }
+        public double Side2
+        {
+            get => side2;
+            set => side2 = value;
+        }
+        public double Side3
+        {
+            get => side3;
+            set => side3 = value;
+        }
+        public double CalArea()
+        {
+            if (!IsValid())
+                throw new SystemException("输入的边长不合法！");
+            double p = (side1 + side2 + side3) / 2;
+            return Math.Sqrt(p * (p - side1) * (p - side2) * (p - side3));
+        }
+        public bool IsValid()
+        {
+            return (side1>0 &&side2>0 && side3>0 &&
+                  side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1);
+        }
+    }
+
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r= new Rectangle(1,2);
+            r.Width = 3;
+            Console.WriteLine(r.CalArea());
+            IShape shape = r;
+            Console.WriteLine(shape.CalArea());
+            shape = new Square(2);
+            Console.WriteLine(shape.CalArea());
+            shape = new Triangle(1, 2, 3);
+            Console.WriteLine(shape.CalArea());
+        }
+    }
+}
