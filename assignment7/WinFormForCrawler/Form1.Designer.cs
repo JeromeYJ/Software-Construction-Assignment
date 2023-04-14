6e9868b55f81e4ede5c6778914929a077f253238
JeromeYJ <93578822+JeromeYJ@users.noreply.github.com>
2023-04-15 00:46:39 +0800

    第七次作业

---
+++ assignment7/WinFormForCrawler/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WinFormForCrawler
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvInfo = new System.Windows.Forms.DataGridView();
+            this.uRLInfoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.noDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.infoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uRLInfoBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.txtURL);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(8);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1561, 226);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "配置初始URL";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.Location = new System.Drawing.Point(600, 95);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(135, 37);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "开始爬取";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(154, 94);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(422, 38);
+            this.txtURL.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "输入初始URL：";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvInfo);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft YaHei UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(0, 226);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(8);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1561, 655);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "已经爬取的URL和错误的URL信息";
+            // 
+            // dgvInfo
+            // 
+            this.dgvInfo.AutoGenerateColumns = false;
+            this.dgvInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInfo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.noDataGridViewTextBoxColumn,
+            this.dataGridViewTextBoxColumn1,
+            this.infoDataGridViewTextBoxColumn});
+            this.dgvInfo.DataSource = this.uRLInfoBindingSource;
+            this.dgvInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvInfo.Location = new System.Drawing.Point(3, 31);
+            this.dgvInfo.Name = "dgvInfo";
+            this.dgvInfo.RowHeadersWidth = 62;
+            this.dgvInfo.RowTemplate.Height = 32;
+            this.dgvInfo.Size = new System.Drawing.Size(1555, 621);
+            this.dgvInfo.TabIndex = 0;
+            // 
+            // uRLInfoBindingSource
+            // 
+            this.uRLInfoBindingSource.DataSource = typeof(SimpleCrawler.URLInfo);
+            // 
+            // noDataGridViewTextBoxColumn
+            // 
+            this.noDataGridViewTextBoxColumn.DataPropertyName = "No";
+            this.noDataGridViewTextBoxColumn.HeaderText = "序号";
+            this.noDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.noDataGridViewTextBoxColumn.Name = "noDataGridViewTextBoxColumn";
+            this.noDataGridViewTextBoxColumn.Width = 160;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Url";
+            this.dataGridViewTextBoxColumn1.HeaderText = "URL";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 8;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 800;
+            // 
+            // infoDataGridViewTextBoxColumn
+            // 
+            this.infoDataGridViewTextBoxColumn.DataPropertyName = "Info";
+            this.infoDataGridViewTextBoxColumn.HeaderText = "状态信息";
+            this.infoDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.infoDataGridViewTextBoxColumn.Name = "infoDataGridViewTextBoxColumn";
+            this.infoDataGridViewTextBoxColumn.Width = 700;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1561, 881);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uRLInfoBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private DataGridViewTextBoxColumn urlDataGridViewTextBoxColumn;
+        private Label label1;
+        private TextBox txtURL;
+        private Button btnStart;
+        private DataGridView dgvInfo;
+        private BindingSource uRLInfoBindingSource;
+        private DataGridViewTextBoxColumn noDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private DataGridViewTextBoxColumn infoDataGridViewTextBoxColumn;
+    }
+}
---
+++ assignment7/assignment7/SimpleCrawler.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Net;
+using System.Security;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SimpleCrawler
+{
+    //包含爬取信息的类
+    public class URLInfo
+    {
+        public int No { get; set; }
+        public string Url { get; set; }
+        public string Info { get; set; }
+        public URLInfo(int no, string url, string info)
+        {
+            No = no;
+            Url = url;
+            Info = info;
+        }
+    }
+    public class SimpleCrawler
+    {
+        public delegate void DispURL(URLInfo url);
+        public event DispURL DispURLEvent;
+        private Queue<string> waiting;
+        public Dictionary<string, bool> urls;
+        private int count;
+        private int maxCount;
+
+        //协议段
+        public string protocol { get; set; }
+        //域名段
+        public string domain { get; set; }
+        //协议与域名段
+        public string site { get; set; }
+        //文件段
+        public string file { get; set; }
+        public string pattern { get; set; }
+
+        public SimpleCrawler()
+        {
+            waiting = new Queue<string>();
+            urls = new Dictionary<string, bool>();
+            count = 0;
+            maxCount = 50;
+        }
+
+        public void Start(string InitURL)
+        {
+            string startUrl = InitURL;
+
+            pattern = @"(?<site>(?<protocol>https?)://(?<domain>[\w\d.-]+)(:\d+)?($|/))(\w+/)*(?<file>[^#]*)";
+            protocol = Regex.Match(startUrl, pattern).Groups["protocol"].Value;
+            domain = Regex.Match(startUrl, pattern).Groups["domain"].Value;
+            file = Regex.Match(startUrl, pattern).Groups["file"].Value;
+            site = Regex.Match(startUrl, pattern).Groups["site"].Value;
+
+            count = 0;
+            waiting.Enqueue(startUrl);
+            new Thread(Crawl).Start();   //开始爬行
+        }
+
+        private void Crawl()
+        {
+            Console.WriteLine("开始爬行了……");
+            while (count < maxCount && waiting.Count > 0)
+            {
+                string current = waiting.Dequeue();
+                Console.WriteLine("爬行 " + current + " 页面！");
+                count++;
+                
+                string html = Download(current);
+                urls[current] = true;
+                URLInfo u = new URLInfo(count, current, "success");
+                DispURLEvent(u);   //触发事件
+                Parse(html, current);
+            }
+            Console.WriteLine("爬行结束");
+        }
+
+        public string Download(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+
+                string fileName = count.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                return html;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                urls[url] = false;
+                URLInfo u = new URLInfo(count, url, e.Message);
+                DispURLEvent(u);   //触发事件
+                return "";
+            }
+        }
+
+        public void Parse(string html, string url)
+        {
+            string strRef = @"(href|HREF)\s*=\s*[""'][^""'#>]+[""']";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                strRef = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim('\"', '"', '#', '>');
+                if (strRef == null || strRef == "" || strRef.StartsWith("javascript:")) 
+                    continue;
+                
+                if(strRef.StartsWith("//"))
+                    strRef = protocol + ":" + strRef;
+                else if(strRef.StartsWith("/"))
+                    strRef = site.EndsWith("/") ? site + strRef.Substring(1) : site + strRef;
+
+
+                string pattern1 = @".(htm|html|aspx|php|jsp)$|^[^.]*$";
+                Match m = Regex.Match(strRef, pattern);
+                if (m.Groups["domain"].Value == domain && 
+                    m.Groups["protocol"].Value == protocol &&
+                    Regex.IsMatch(m.Groups["file"].Value, pattern1) &&
+                    !waiting.Contains(strRef))
+                {
+                    waiting.Enqueue(strRef);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            SimpleCrawler myCrawler = new SimpleCrawler();
+            string startUrl = "https://baike.baidu.com";
+
+            myCrawler.Start(startUrl);
+        }
+    }
+}
